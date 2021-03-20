94f907e164860007026016a92caf50f9aeaa5591
Aghasi <aghasidokhoyan@gmail.com>
2021-03-20 07:05:59 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+######################################
+#Copyright of David Bombal, 2021     #
+#https://www.davidbombal.com         #
+#https://www.youtube.com/davidbombal #
+######################################
+
+# Import subprocess so we can use system commands
+import subprocess
+
+# Import the re module so that we can make use of regular expressions. 
+import re
+import time
+
+
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+
+time.sleep(3600)
+profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+
+
+wifi_list = list()
+
+
+
+if len(profile_names) != 0:
+    for name in profile_names:
+       
+        wifi_profile = dict()
+
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+      
+        if re.search("Security key           : Absent", profile_info):
+            continue
+        else:
+
+            wifi_profile["ssid"] = name
+            
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+
+            password = re.search("Key Content            : (.*)\r", profile_info_pass)
+            
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+                
+                wifi_profile["password"] = password[1]
+ 
+            wifi_list.append(wifi_profile) 
+
+for x in range(len(wifi_list)):
+    print(wifi_list[x]) 
